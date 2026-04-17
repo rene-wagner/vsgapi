1e0e063dbcce199cc3b9c91692a131b0f0ec1fae
René Wagner <rene.papenfuss@getaway.group>
2026-04-17 10:09:14 +0200

    feat: add nelmio/cors-bundle for CORS support and configure CORS settings

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
